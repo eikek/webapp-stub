f1d5ce1b489b613206beb946c6c478d80a4ac03a
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2024-09-11 06:15:35 +0000

    Update scala3-library to 3.4.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys.scalaVersion
 object Dependencies {
 
   object V {
-    val scala3 = "3.4.2"
+    val scala3 = "3.4.3"
 
     val borer = "1.14.1"
     val borerCompats = "0.1.0-SNAPSHOT"
